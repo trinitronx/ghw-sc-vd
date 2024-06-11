e2299f3e1f12d05aee47fc1b46a67b17a1792c76
James Cuzella <james.cuzella@lyraphase.com>
2024-06-11 16:04:33 -0600

    Add generic helper script to run any command under dotenvx

---
+++ bin/npx-dotenvx-run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+SELF_DIR=$(cd $(dirname $0) && pwd)
+
+set -e # Drop-out from execution if error occurs
+
+if [ -z "$LP_WALLET_CUSTOM_ITEM_ID" ]; then
+  read -r -s -p 'Enter ETH Wallet LastPass ID: ' LP_WALLET_CUSTOM_ITEM_ID
+  [ -z "$LP_WALLET_CUSTOM_ITEM_ID" ] && echo "No LastPass ID provided" && exit 1
+  export LP_WALLET_CUSTOM_ITEM_ID
+fi
+
+npx  dotenvx run --env-file=.env.dev -- $@
